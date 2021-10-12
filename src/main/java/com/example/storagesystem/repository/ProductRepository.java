307e47b402da0352377c4b775a3d801d0176fca5
nasosvava <nasosvava@gmail.com>
2021-10-12 15:22:52 +0300

    Service Repositories

--- src/main/java/com/example/storagesystem/repository/ProductRepository.java
+++ src/main/java/com/example/storagesystem/repository/ProductRepository.java
@@ -2,7 +2,6 @@ package com.example.storagesystem.repository;
 
 
 import com.example.storagesystem.domain.Product;
-import com.example.storagesystem.dto.ProductDTO;
 import com.example.storagesystem.repository.customrepository.CustomProductRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
